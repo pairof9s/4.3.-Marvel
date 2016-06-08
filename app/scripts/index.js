ebb7f2510ed6b889715336d2146a64bd6e3ca786
David Nine <pairof9s@gmail.com>
2016-06-08 12:38:00 -0400

    morning notes

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+//console.log("Hello World!");
+
+var $ = require('jquery');
+
+window.setTimeout(function(){  // This "stack" or full function is executed after performing first task of "Testing 2..."
+  console.log('Testing..');
+}, 0);
+
+console.log('Testing 2...');  // Actual initial event triggered, before function above is called
